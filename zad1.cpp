aa60c708f379fd7322c454439b644291ad2ca1ef
ficerman <89976352+ficerman@users.noreply.github.com>
2021-09-29 23:35:00 +0200

    Add files via upload

---
+++ zad1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int tab[2][2];
+int main()
+{
+	for (int i = 0; i < 2; i++) {
+		for (int i2 = 0; i2 < 2; i2++) {
+			cout << endl << "kolumna " << i + 1 << " wiersz " << i2 + 1 << ":" << endl;
+			cin >> tab[i][i2];
+		}
+	}
+}
---
+++ zad4.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int tab[10][10];
+int main()
+{
+	for (int i = 0; i < 10; i++) {
+		for (int i2 = 0; i2 < 10; i2++) {
+			if (i = i2) {
+				tab[i][i2] = i;
+			}
+			else {
+				tab[i][i2] = 0;
+			}
+		}
+	}
+	for (int i = 0; i < 10; i++) {
+		for (int i2 = 0; i2 < 10; i2++) {
+			cout << tab[i][i2];
+		}
+		cout << endl;
+	}
+}
